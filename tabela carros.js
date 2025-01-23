67195bb3892c836627cb0b4a4a16c73982fb2831
Luccao1 <ranyelucas7@gmail.com>
2025-01-23 15:05:11 -0300

    thomas

---
+++ tabela carros.js
@@ -0,0 +1,22 @@
+const carros2025 =[
+    {
+        nome: "ferrari",
+        sigla: "fer",
+        velocidademaxima: 340,
+        potencia: 800,
+        consumo: 5.5,
+        aceleracao: 2.9,
+        preço: 5000000000,
+    },
+    {
+        nome: "bugati",
+        sigla: "bug",
+        velocidademaxima: 500,
+        potencia: 1500,
+        consumo: 10.0,
+        aceleracao: 2.0,
+        preço: 100000000,
+    }, 
+];    
+
+export default carros2025;
